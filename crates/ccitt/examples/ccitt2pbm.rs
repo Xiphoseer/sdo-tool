24b1dc56885d2043a92a5b2775cdedf335b5820d
Xiphoseer <xiphoseer@mailbox.org>
2022-02-13 14:13:44 +0100

    Support make up codes

---
+++ crates/ccitt/examples/ccitt2pbm.rs
@@ -0,0 +1,49 @@
+use std::path::PathBuf;
+
+use ccitt_t4_t6::{bit_iter::BitIter, bit_iter::BitWriter, g42d::decode::Decoder};
+use color_eyre::eyre;
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Options {
+    file: PathBuf,
+    #[structopt(long)]
+    width: usize,
+    #[structopt(long, short)]
+    invert: bool,
+    #[structopt(long)]
+    debug: bool,
+}
+
+fn main() -> eyre::Result<()> {
+    color_eyre::install()?;
+    let opt = Options::from_args();
+    let file = std::fs::read(&opt.file)?;
+
+    let mut decoder = Decoder::<BitWriter>::new(opt.width);
+    decoder.debug = opt.debug;
+    decoder.decode(&file)?;
+    let store = decoder.into_store();
+
+    let bitmap = store.done();
+    let mut iter = BitIter::new(&bitmap);
+
+    let width = opt.width;
+    let height = bitmap.len() * 8 / width;
+
+    println!("P1 {} {}", width, height);
+
+    for _ in 0..height {
+        for _ in 0..width {
+            let bit = iter.next().unwrap();
+            if bit ^ opt.invert {
+                print!("0");
+            } else {
+                print!("1");
+            }
+        }
+        println!();
+    }
+
+    Ok(())
+}
