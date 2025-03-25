9983ca69354fb62c34b2f956483a3b74652bbcdc
Xiphoseer <xiphoseer@mailbox.org>
2025-03-25 10:01:50 +0100

    feat: always parse foused

--- crates/signum/src/docs/v3/mod.rs
+++ crates/signum/src/docs/v3/mod.rs
@@ -6,9 +6,9 @@ use std::{borrow::Cow, num::NonZeroU32};
 
 use nom::{
     bytes::complete::{tag, take, take_while},
-    combinator::{map_parser, rest},
-    error::ParseError,
-    multi::{length_value, many0},
+    combinator::{map, map_parser, rest, verify},
+    error::{context, ContextError, ParseError},
+    multi::{length_value, many0, many1},
     number::complete::{be_u16, be_u32},
     sequence::{preceded, terminated},
     IResult, Parser,
@@ -19,18 +19,40 @@ use crate::{chsets::encoding::decode_atari_str, util::Buf};
 /// Tag for a v3 document
 pub const TAG_SDOC3: &[u8; 12] = b"\0\0sdoc  03\0\0";
 
-const CHUNK_FLPTRS01: &[u8; 12] = b"\0\0flptrs01\0\0";
+/// Tag for *file pointers*
+pub const CHUNK_FLPTRS01: &[u8; 12] = b"\0\0flptrs01\0\0";
 /// Tag for *fonts used*
 pub const CHUNK_FOUSED01: &[u8; 12] = b"\0\0foused01\0\0";
+
+/// Header of a document
+#[derive(Debug)]
+#[allow(dead_code)]
+pub struct Header<'a> {
+    buf: Buf<'a>,
+}
 
 /// Document root
 #[allow(dead_code)]
 #[derive(Debug)]
 pub struct SDocV3<'a> {
     /// Header buffer
-    pub header: Buf<'a>,
+    header: Header<'a>,
     /// The file pointers
-    pub file_pointers: FilePointers,
+    file_pointers: FilePointers,
+    /// The fonts in the file
+    fonts: FontsUsed<'a>,
+}
+
+impl<'a> SDocV3<'a> {
+    /// Get the *file pointers* `flptrs01` chunk
+    pub fn flptrs01(&self) -> &FilePointers {
+        &self.file_pointers
+    }
+
+    /// Get the *fonts used* `foused01` chunk
+    pub fn foused01(&self) -> &FontsUsed<'a> {
+        &self.fonts
+    }
 }
 
 /// `flptrs01` chunk
@@ -38,9 +60,9 @@ pub struct SDocV3<'a> {
 #[derive(Debug)]
 pub struct FilePointers {
     /// Offset of `foused01`
-    pub ofs_foused01: Option<NonZeroU32>,
+    pub ofs_foused01: u32,
     /// Offset of `params01`
-    pub ofs_params01: Option<NonZeroU32>,
+    pub ofs_params01: u32,
     /// Offset of `cdilist0`
     pub ofs_cdilist0: Option<NonZeroU32>,
     /// Offset of `foxlist `
@@ -49,29 +71,37 @@ pub struct FilePointers {
     u6: Option<NonZeroU32>,
     u7: Option<NonZeroU32>,
     u8: Option<NonZeroU32>,
-    /// Offset of first `kapit 01`
-    pub ofs_content_first: Option<NonZeroU32>,
-    /// Offset of last `kapit 01`
-    pub ofs_content_last: Option<NonZeroU32>,
+    /// Offsets of `kapit 01`
+    pub ofs_chapters: Vec<u32>,
 }
 
-fn parse_header<'a, E: ParseError<&'a [u8]>>(input: &'a [u8]) -> IResult<&'a [u8], Buf<'a>, E> {
-    let (input, rest) = rest(input)?;
-    Ok((input, Buf(rest)))
+fn parse_header<'a, E: ParseError<&'a [u8]>>(input: &'a [u8]) -> IResult<&'a [u8], Header<'a>, E> {
+    let (input, buf) = map(rest, Buf)(input)?;
+    Ok((input, Header { buf }))
 }
 
-fn be_u32_bounded<'a, E: ParseError<&'a [u8]>>(
+fn opt_be_nonzero_u32<'a, E: ParseError<&'a [u8]>>(
     input: &'a [u8],
 ) -> IResult<&'a [u8], Option<NonZeroU32>, E> {
-    if input.is_empty() {
-        Ok((input, None))
-    } else {
-        let (input, val) = be_u32(input)?;
-        Ok((input, NonZeroU32::new(val)))
-    }
+    let (input, val) = be_u32(input)?;
+    Ok((input, NonZeroU32::new(val)))
 }
 
-type FontUsed<'a> = (u8, Cow<'a, str>);
+/// A single used font
+pub type FontUsed<'a> = (u8, Cow<'a, str>);
+
+/// `foused01` chunk
+#[derive(Debug)]
+pub struct FontsUsed<'a> {
+    fonts: Vec<FontUsed<'a>>,
+}
+
+impl<'a> FontsUsed<'a> {
+    /// Get all fonts
+    pub fn fonts(&self) -> &[FontUsed<'a>] {
+        &self.fonts
+    }
+}
 
 fn parse_font_used<'a, E: ParseError<&'a [u8]>>(
     input: &'a [u8],
@@ -84,23 +114,22 @@ fn parse_font_used<'a, E: ParseError<&'a [u8]>>(
 /// Parse a `foused01` chunk
 pub fn parse_foused01<'a, E: ParseError<&'a [u8]>>(
     input: &'a [u8],
-) -> IResult<&'a [u8], Vec<FontUsed<'a>>, E> {
-    many0(parse_font_used)(input)
+) -> IResult<&'a [u8], FontsUsed<'a>, E> {
+    map(many0(parse_font_used), |fonts| FontsUsed { fonts })(input)
 }
 
 fn parse_flptrs01<'a, E: ParseError<&'a [u8]>>(
     input: &'a [u8],
 ) -> IResult<&'a [u8], FilePointers, E> {
-    let (input, ofs_foused01) = be_u32_bounded(input)?;
-    let (input, ofs_params01) = be_u32_bounded(input)?;
-    let (input, ofs_cdilist0) = be_u32_bounded(input)?;
-    let (input, ofs_foxlist) = be_u32_bounded(input)?;
-    let (input, u5) = be_u32_bounded(input)?;
-    let (input, u6) = be_u32_bounded(input)?;
-    let (input, u7) = be_u32_bounded(input)?;
-    let (input, u8) = be_u32_bounded(input)?;
-    let (input, ofs_content_first) = be_u32_bounded(input)?;
-    let (input, ofs_content_last) = be_u32_bounded(input)?;
+    let (input, ofs_foused01) = verify(be_u32, |v| *v > 0)(input)?;
+    let (input, ofs_params01) = verify(be_u32, |v| *v > 0)(input)?;
+    let (input, ofs_cdilist0) = opt_be_nonzero_u32(input)?;
+    let (input, ofs_foxlist) = opt_be_nonzero_u32(input)?;
+    let (input, u5) = opt_be_nonzero_u32(input)?;
+    let (input, u6) = opt_be_nonzero_u32(input)?;
+    let (input, u7) = opt_be_nonzero_u32(input)?;
+    let (input, u8) = opt_be_nonzero_u32(input)?;
+    let (input, ofs_chapters) = many1(be_u32)(input)?;
     Ok((
         input,
         FilePointers {
@@ -112,14 +141,13 @@ fn parse_flptrs01<'a, E: ParseError<&'a [u8]>>(
             u6,
             u7,
             u8,
-            ofs_content_first,
-            ofs_content_last,
+            ofs_chapters,
         },
     ))
 }
 
 /// Parse a chunk
-pub fn parse_chunk_head<'a, T, F, E: ParseError<&'a [u8]>>(
+fn parse_chunk_head<'a, T, F, E: ParseError<&'a [u8]>>(
     chunk_tag: &'static [u8; 12],
     parser: F,
 ) -> impl FnMut(&'a [u8]) -> IResult<&'a [u8], T, E>
@@ -130,24 +158,24 @@ where
     preceded(tag(chunk_tag), p)
 }
 
-/// Parse the *fonts used* chunk
-pub fn parse_chunk_foused01<'a, E: ParseError<&'a [u8]>>(
-    input: &'a [u8],
-) -> IResult<&'a [u8], Vec<FontUsed<'a>>, E> {
-    parse_chunk_head(CHUNK_FOUSED01, parse_foused01)(input)
-}
-
 /// Parse a Signum! document
-pub fn parse_sdoc_v3<'a, E: ParseError<&'a [u8]>>(
-    input: &'a [u8],
-) -> IResult<&'a [u8], SDocV3<'a>, E> {
+pub fn parse_sdoc_v3<'a, E>(input: &'a [u8]) -> IResult<&'a [u8], SDocV3<'a>, E>
+where
+    E: ParseError<&'a [u8]>,
+    E: ContextError<&'a [u8]>,
+{
+    let data = input;
     let (input, header) = parse_chunk_head(TAG_SDOC3, parse_header)(input)?;
     let (input, file_pointers) = parse_chunk_head(CHUNK_FLPTRS01, parse_flptrs01)(input)?;
+    let (i_foused01, _) = take(file_pointers.ofs_foused01)(data)?;
+    let (_, fonts) =
+        context("foused01", parse_chunk_head(CHUNK_FOUSED01, parse_foused01))(i_foused01)?;
     Ok((
         input,
         SDocV3 {
             header,
             file_pointers,
+            fonts,
         },
     ))
 }
